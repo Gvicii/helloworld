67765a0d30bd0fceed2a91547226096226e31f44
Gvicii <635219677@qq.com>
2020-05-11 14:26:37 +0800

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,6 +1,6 @@
 public class HiWorld {
     public static void main(String[] args) {
         System.out.println("hi world");
-        System.out.println("github is laji!");
+        System.out.println("github is laji! ");
     }
 }
