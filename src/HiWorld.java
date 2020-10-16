584dbfb65234d56fc96b2e5157e798f7270e29fc
Gvicii <635219677@qq.com>
2020-10-16 15:30:46 +0800

    this is a test edit

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,6 +1,6 @@
 public class HiWorld {
     public static void main(String[] args) {
         System.out.println("hi world");
-        System.out.println("wang su yang like  二刺猿");
+        System.out.println("wang su yang like       二刺猿");
     }
 }
